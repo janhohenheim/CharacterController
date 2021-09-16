4a392ef6081c339d6033af28fb62cd7c433371ec
Jan Nils Ferner <contact@jnferner.com>
2021-09-16 14:32:36 +0200

    Implement gravity

--- Assets/Scripts/CharacterControllerSystem.cs
+++ Assets/Scripts/CharacterControllerSystem.cs
@@ -1,9 +1,11 @@
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using Unity.Transforms;
+using Utils;
 
 [UpdateAfter(typeof(ExportPhysicsWorld)), UpdateBefore(typeof(EndFramePhysicsSystem))]
 public class CharacterControllerSystem : JobComponentSystem
@@ -55,7 +57,7 @@ public class CharacterControllerSystem : JobComponentSystem
             EntityHandles = entityTypeHandle,
             ColliderData = colliderData,
 
-            CharacterControllerType = characterControllerTypeHandle,
+            CharacterControllerHandles = characterControllerTypeHandle,
             TranslationHandles = translationTypeHandle,
             RotationHandles = rotationTypeHandle
         };
@@ -68,6 +70,7 @@ public class CharacterControllerSystem : JobComponentSystem
         return controllerJobHandle;
     }
 
+    // [BurstCompile]
     private struct CharacterControllerJob : IJobChunk
     {
         public float DeltaTime;
@@ -76,14 +79,85 @@ public class CharacterControllerSystem : JobComponentSystem
         [ReadOnly] public EntityTypeHandle EntityHandles;
         [ReadOnly] public ComponentDataFromEntity<PhysicsCollider> ColliderData;
 
-        public ComponentTypeHandle<CharacterControllerComponent> CharacterControllerType;
+        public ComponentTypeHandle<CharacterControllerComponent> CharacterControllerHandles;
         public ComponentTypeHandle<Translation> TranslationHandles;
         public ComponentTypeHandle<Rotation> RotationHandles;
 
 
         public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
         {
-            throw new System.NotImplementedException();
+            var collisionWorld = PhysicsWorld.CollisionWorld;
+            var chunkEntityData = chunk.GetNativeArray(EntityHandles);
+            var chunkCharacterControllerData = chunk.GetNativeArray(CharacterControllerHandles);
+            var chunkTranslationData = chunk.GetNativeArray(TranslationHandles);
+            var chunkRotationData = chunk.GetNativeArray(RotationHandles);
+
+            for (var i = 0; i < chunk.Count; i++)
+            {
+                var entity = chunkEntityData[i];
+                var controller = chunkCharacterControllerData[i];
+                var position = chunkTranslationData[i];
+                var rotation = chunkRotationData[i];
+                var collider = ColliderData[entity];
+
+                HandleChunk(ref entity, ref controller, ref position, ref rotation, ref collider, ref collisionWorld);
+                chunkTranslationData[i] = position;
+                chunkCharacterControllerData[i] = controller;
+            }
+        }
+
+        private void HandleChunk(ref Entity entity, ref CharacterControllerComponent controller, ref Translation position, ref Rotation rotation, ref PhysicsCollider collider, ref CollisionWorld collisionWorld)
+        {
+            var epsilon = new float3(0.0f, Epsilon, 0.0f) * -math.normalize(controller.Gravity);
+            var currPos = position.Value + epsilon;
+            var currRot = rotation.Value;
+
+            var verticalVelocity = new float3();
+            var jumpVelocity = new float3();
+            var gravityVelocity = controller.IsGrounded ? 0.0f : controller.Gravity * DeltaTime;
+            
+            HandleVerticalMovement(ref verticalVelocity, ref jumpVelocity, ref gravityVelocity, ref entity, ref currPos, ref currRot, ref controller, ref collider, ref collisionWorld);
+            currPos += verticalVelocity;
+            position.Value = currPos - epsilon;
+        }
+
+        private void HandleVerticalMovement(ref float3 totalVelocity, ref float3 jumpVelocity, ref float3 gravityVelocity, ref Entity entity, ref float3 currPos, ref quaternion currRot, ref CharacterControllerComponent controller, ref PhysicsCollider collider, ref CollisionWorld collisionWorld)
+        {
+            totalVelocity = jumpVelocity + gravityVelocity;
+
+            var verticalCollisions =
+                PhysicsUtils.ColliderCastAll(collider, currPos, currPos + totalVelocity, ref collisionWorld, entity, Allocator.Temp);
+            PhysicsUtils.TrimByFilter(ref verticalCollisions, ColliderData, PhysicsCollisionFilters.DynamicWithPhysical);
+
+            if (verticalCollisions.Length != 0)
+            {
+                var transform = new RigidTransform
+                {
+                    pos = currPos + totalVelocity,
+                    rot = currRot,
+                };
+
+                if (PhysicsUtils.ColliderDistance(out var verticalPenetration, collider, 1.0f, transform,
+                    ref collisionWorld, entity, PhysicsCollisionFilters.DynamicWithPhysical, null, ColliderData,
+                    Allocator.Temp))
+                {
+                    if (verticalPenetration.Distance < 0.0f)
+                    {
+                        totalVelocity += verticalPenetration.SurfaceNormal * -verticalPenetration.Distance;
+
+                        if (PhysicsUtils.ColliderCast(out var adjustedHit, collider, currPos, currPos + totalVelocity,
+                            ref collisionWorld, entity, PhysicsCollisionFilters.DynamicWithPhysical, null, ColliderData,
+                            Allocator.Temp))
+                        {
+                            totalVelocity *= adjustedHit.Fraction;
+                        }
+
+                        controller.JumpVelocity = new float3();
+                    }
+                }
+            }
+
+            totalVelocity = MathUtils.ZeroOut(totalVelocity, 0.01f);
         }
     }
 }
